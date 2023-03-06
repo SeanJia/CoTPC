c128d6a244fbc26b0e84b19ab796132f255bc9ca
SeanJia <zjia@eng.ucsd.edu>
2023-03-06 12:39:03 -0800

    added parallel evaluation with vec_env

--- src/eval.py
+++ src/eval.py
@@ -2,9 +2,20 @@ import os
 import gym
 import torch
 import numpy as np
+import argparse
+from tqdm import tqdm
+from collections import defaultdict
 
-from data import MS2Demos
-from model import GPTConfig, GPTWithCoT
+from mani_skill2.utils.io_utils import load_json
+
+import mani_skill2.envs  # To load ManiSkill2 envs.
+from model_bak import GPTConfig, GPTWithCoT
+
+from vec_env import get_mp_envs  # Used for parallel evaluation.
+
+# Please specify the model and data path (base folder).
+MODEL_PATH = '/home/zjia/Research/inter_seq/CoTPC/models'
+DATA_PATH = '/home/zjia/Research/inter_seq/data/rigid_body_envs'  
 
 
 @torch.no_grad()
@@ -22,7 +33,7 @@ def predict(model, action_hist, state_hist, t):
     # T is the max sequence size; S is the current number of steps.
     B, T = states.shape[0], model.block_size + model.len_key_states
     n_head, S = model.config.n_head, states.shape[1] - 1  # Exclude the init state.
-    
+
     # Masks for the all-to-all key state query tokens in attention layers.
     # The built-in masks for causal (auto-regressive) tokens are in `model.py`.
     key_state_mask = torch.zeros([B, n_head, T, T], dtype=bool)
@@ -31,9 +42,10 @@ def predict(model, action_hist, state_hist, t):
     m3 = m1 > m2  # Tokens in the future are masked out.
     m3 = m3[:, None, None, :].repeat(1, n_head, model.len_key_states, 1)
     key_state_mask[:, :, :model.len_key_states, :] = m3
+    key_state_mask = key_state_mask.cuda()
     preds, _ = model(
         states, timesteps, actions=actions, key_state_mask=key_state_mask)
-    return preds[:, -1]  # Only output the action predictions.
+    return preds[:, -1]  # Only output the last action predictions.
 
 
 def update(model, action_hist, state_hist, actions, states, t):
@@ -53,28 +65,136 @@ def update(model, action_hist, state_hist, actions, states, t):
     return action_hist, state_hist, t
 
 
-if __name__ == "__main__":
-    conf = GPTConfig(
-        "Please specify required parameters",  
-        # train_data.max_steps
-        max_timestep="The max sequence length used during training (for position embedding.)")
-    model = GPTWithCoT(
-        conf, state_dim='Can use train_data.info()', action_dim='Can use train_data.info()').cuda()
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-    model_path = "Specify the model ckpt path"
-    model.load_state_dict(torch.load(model_path), strict=False)
+    # Hyper-parameters regarding the demo dataset (used to gather eval_ids)
+    parser.add_argument('--task', type=str, default='PickCube-v0', help="Task (env-id) in ManiSkill2.")
+    parser.add_argument('--control_mode', type=str, default='pd_joint_delta_pos', 
+                        help="Control mode used in envs from ManiSkill2.")
+    parser.add_argument('--obs_mode', type=str, default='state', 
+                        help="State mode used in envs from ManiSkill2.")
+    parser.add_argument("--seed", default=0, type=int,help="Random seed for data spliting.")
+    parser.add_argument("--num_traj", default=-1, type=int, help="Number of training trajectories.")
+
+    # Hyper-parameters regarding the model.
+    parser.add_argument('--context_length', type=int, default=60, 
+                        help="Context size of CoTPC.")
+    parser.add_argument("--n_layer", default=4, type=int, help="Number of attention layers.")
+    parser.add_argument("--n_head", default=8, type=int, help="Number of attention heads.")
+    parser.add_argument("--n_embd", default=128, type=int, help="Hidden feature dimension.")
+    parser.add_argument('--model_type', type=str, default='s+a+cot', 
+                        help="Model type for the CoTPC model (see GPTConfig).")
+    parser.add_argument('--key_states', type=str, default='a', 
+                        help="Which key states to use (see GPTConfig for the spec. format).")
+    parser.add_argument("--key_state_loss", default='', type=str, 
+                        help="Features out of what attention layers to use for key state prediction " +
+                        "losses (see GPTConfig for the spec. format).")
+    parser.add_argument("--max_steps", default=-1, type=int, 
+                        help="Max steps of seqences in training data (for loading the pos enbedding).")
+    parser.add_argument("--model_name", default='', type=str, help="Model name to be loaded.")
+    parser.add_argument("--from_ckpt", default=-1, type=int, help="Ckpt of the model to be loaded.")
+    parser.add_argument("--state_dim", default=-1, type=int, help="Dim of the state space.")
+    parser.add_argument("--action_dim", default=8, type=int, help="Dim of the action space.")
+    
+    parser.add_argument("--eval_max_steps", default=200, type=int, help="Max steps allowed in eval.")
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    assert args.max_steps > 0, 'Should specify --max_steps'
+    assert args.model_name, 'Should specify --model_name'
+    assert args.from_ckpt > 0, 'Should specify --from_ckpt'
+    assert args.state_dim > 0, 'Should specify --state_dim'
+
+    # Load the model.
+    conf = GPTConfig(
+        args.context_length, 
+        n_layer=args.n_layer, 
+        n_head=args.n_head, 
+        n_embd=args.n_embd, 
+        model_type=args.model_type, 
+        key_states=args.key_states,
+        key_state_loss=args.key_state_loss,
+        max_timestep=args.max_steps,
+    )
+    model = GPTWithCoT(conf, state_dim=args.state_dim, action_dim=args.action_dim).cuda()
+    path = os.path.join(MODEL_PATH, f'{args.model_name}/{args.from_ckpt}.pth')
+    print('Loaded ckpt from:', path)  
+    model.load_state_dict(torch.load(path), strict=False) 
     model.eval()
 
-    # An example eval loop.
-    env = gym.make('Some inputs here.')
-    s = env.reset()
-    state_hist, action_hist, t = [s], [], np.zeros([1])
+    # Load demos to fetch the env. seeds used in training.
+    json_path = os.path.join(
+        DATA_PATH, f'{args.task}/trajectory.{args.obs_mode}.{args.control_mode}.json')
+    json_data = load_json(json_path)
+    env_kwargs = json_data["env_info"]["env_kwargs"]
+    env_kwargs["obs_mode"] = args.obs_mode
+    env_kwargs["control_mode"] = args.control_mode
+    np.random.seed(args.seed)
+    if args.task == 'TurnFaucet-v0':
+        length_all = len(json_data["episodes"])
+        ids = []
+        for i in range(10):  # Hard-code the 10 data splits for permutation.
+            t_ids = np.random.permutation(length_all//10)[:args.num_traj//10]
+            t_ids += i*length_all//10
+            ids.append(t_ids)
+        eval_ids = np.concatenate(ids)
+    else:
+        eval_ids = np.random.permutation(len(json_data["episodes"]))[:args.num_traj]
 
-    for step in range(200):  # Max steps.
-        a = predict(model, action_hist, state_hist, t).cpu().numpy()[0]
+    # Number of parallel environments.
+    n_env = 25
+    assert len(eval_ids) % n_env == 0, f'{len(eval_ids)}'
+    envs = get_mp_envs(args.task, n_env, **env_kwargs)
+
+    metric_dict = defaultdict(lambda: [[] for _ in range(len(eval_ids))])
+
+    for start_idx in tqdm(range(0, len(eval_ids), n_env)):
+        reset_args_list = []
+        for i in range(start_idx, min(start_idx + n_env, len(eval_ids))):
+            reset_kwargs = {'seed': json_data["episodes"][eval_ids[i]]["episode_seed"]}
+            reset_args_list.append(reset_kwargs)
+
+        s = torch.from_numpy(envs.reset(reset_args_list)).float()
+        state_hist, action_hist, t = [s], [], np.zeros([n_env])
+
+        for step in range(args.eval_max_steps):
+            a = predict(model, action_hist, state_hist, t).cpu().numpy()
+
+            s, _, _, infos = envs.step(a)
+            s = torch.from_numpy(s).float()
+            
+            action_hist, state_hist, t = update(
+                model, action_hist, state_hist, a, s, t)
+            
+            # Update metrics.
+            for i, info in enumerate(infos):
+                j = start_idx + i            
+                if args.task == 'StackCube-v0':
+                    metric_dict['is_cubaA_grasped'][j].append(info['is_cubaA_grasped'])
+                    metric_dict['is_cubeA_on_cubeB'][j].append(info['is_cubeA_on_cubeB'])
+                metric_dict['success'][j].append(info['success'])
+            
+    output_str = ''
+    for k, v in metric_dict.items():
+        v = np.mean([np.any(vv) for vv in v]) * 100
+        output_str += f'{k} {v:.2f}, '
+    output_str = output_str[:-2]
+    print(output_str)
+
+    # Example eval loop with a single env (not the paralleled VecEnv).
+    # env = gym.make('Some inputs here.')
+    # s = env.reset()
+    # state_hist, action_hist, t = [s], [], np.zeros([1])
+
+    # for step in range(args.eval_max_steps):
+    #     a = predict(model, action_hist, state_hist, t).cpu().numpy()[0]
         
-        s, _, _, info = env.step(a)
-        s = torch.from_numpy(s).float()[None, :]
-        a = a[None, :]
+    #     s, _, _, info = env.step(a)
+    #     s = torch.from_numpy(s).float()[None, :]
+    #     a = a[None, :]
         
-        action_hist, state_hist, t = update(model, action_hist, state_hist, a, s, t)
+    #     action_hist, state_hist, t = update(model, action_hist, state_hist, a, s, t)
---
+++ src/vec_env.py
@@ -0,0 +1,104 @@
+import pickle
+import cloudpickle
+import numpy as np
+from multiprocessing import Pipe, Process
+import gym
+
+
+def get_mp_envs(env_id, n_env, start_idx=0, **env_kwargs):
+    def env_fn(rank):
+        def fn():
+            env = gym.make(env_id, **env_kwargs)
+            return env
+        return fn
+    return VecEnv([env_fn(i + start_idx) for i in range(n_env)])
+
+
+class CloudpickleWrapper(object):
+	def __init__(self, x):
+		self.x = x
+
+	def __getstate__(self):
+		return cloudpickle.dumps(self.x)
+
+	def __setstate__(self, ob):
+		self.x = pickle.loads(ob)
+	
+	def __call__(self):
+		return self.x()
+
+def worker(remote, parent_remote, env_fn):
+	parent_remote.close()
+	env = env_fn()
+	while True:
+		cmd, data = remote.recv()
+		
+		if cmd == 'step':
+			ob, reward, done, info = env.step(data)
+			# if done: ob = env.reset()  # We ignore the done signal here.
+			remote.send((ob, reward, done, info))
+		elif cmd == 'reset':
+			ob = env.reset(**data)
+			remote.send(ob)
+		elif cmd == 'render':
+			remote.send(env.render())
+		elif cmd == 'close':
+			remote.close()
+			break
+		else:
+			raise NameError('NotImplentedError')
+
+class VecEnv():
+	def __init__(self, env_fns):
+		self.waiting = False
+		self.closed = False
+		no_envs = len(env_fns)
+		self.remotes, self.work_remotes = zip(*[Pipe() for _ in range(no_envs)])
+		self.ps = []
+		
+		for wrk, rem, fn in zip(self.work_remotes, self.remotes, env_fns):
+			p = Process(target=worker, args=(wrk, rem, CloudpickleWrapper(fn)))
+			self.ps.append(p)
+
+		for p in self.ps:
+			p.daemon = True
+			p.start()
+
+		for remote in self.work_remotes:
+			remote.close()
+
+	def step_async(self, actions):
+		if self.waiting:
+			raise NameError('AlreadySteppingError')
+		self.waiting = True
+		for remote, action in zip(self.remotes, actions):
+			remote.send(('step', action))
+	
+	def step_wait(self):
+		if not self.waiting:
+			raise NameError('NotSteppingError')
+		self.waiting = False
+		results = [remote.recv() for remote in self.remotes]
+		obs, rews, dones, infos = zip(*results)
+		return np.stack(obs), np.stack(rews), np.stack(dones), infos
+	
+	def step(self, actions):
+		self.step_async(actions)
+		return self.step_wait()
+	
+	def reset(self, kwargs_list):
+		for remote, kwargs in zip(self.remotes, kwargs_list):
+			remote.send(('reset', kwargs))
+		return np.stack([remote.recv() for remote in self.remotes])
+	
+	def close(self):
+		if self.closed:
+			return
+		if self.waiting:
+			for remote in self.remotes:
+				remote.recv()
+		for remote in self.remotes:
+			remote.send(('close', None))
+		for p in self.ps:
+			p.join()
+		self.closed = True
